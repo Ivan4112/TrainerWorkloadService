9b0807d771efc6b1140555f7cc39c7fda54b9cdc
Ivan4112 <ivanbondar224@gmail.com>
2025-02-03 02:20:24 +0200

    Initial commit

---
+++ TrainerWorkloadService/src/test/java/org/edu/fpm/trainerworkloadservice/TrainerWorkloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.fpm.trainerworkloadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerWorkloadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
